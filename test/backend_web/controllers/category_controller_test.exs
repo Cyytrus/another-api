406b94314d89aa47e14970d4ff59ea33c94a9236
Cyytrus <antoniopaulo122@gmail.com>
2022-02-22 18:16:51 -0300

    Add another test

--- test/backend_web/controllers/category_controller_test.exs
+++ test/backend_web/controllers/category_controller_test.exs
@@ -1,5 +1,6 @@
 defmodule BackendWeb.CategoryControllerTest do
   use BackendWeb.ConnCase
+  alias Backend.Categories
 
   describe "categories" do
     test "list all categories", %{conn: conn} do
@@ -14,8 +15,15 @@ defmodule BackendWeb.CategoryControllerTest do
 
       conn = get(conn, Routes.category_path(conn, :show, id))
 
-      assert %{"id" => id, "description" => "rogerio777", "name" => "SPORT"} =
+      assert %{"id" => ^id, "description" => "rogerio777", "name" => "SPORT"} =
                json_response(conn, 200)
+    end
+
+    test "should throw an error when a category already exist", %{conn: conn} do
+      category = %{name: "Sport", description: "rogerio777"}
+      Categories.create(category)
+      conn = post(conn, Routes.category_path(conn, :create, category: category))
+      assert "has already been taken" in json_response(conn, 422)["errors"]["name"]
     end
   end
 end
