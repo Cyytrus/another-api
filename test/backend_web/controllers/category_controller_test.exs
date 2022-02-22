e54f41a94ba4c4029c520b4a0bcb3751b9a3cae7
Cyytrus <antoniopaulo122@gmail.com>
2022-02-22 18:06:13 -0300

    add more tests

--- test/backend_web/controllers/category_controller_test.exs
+++ test/backend_web/controllers/category_controller_test.exs
@@ -1,10 +1,21 @@
 defmodule BackendWeb.CategoryControllerTest do
   use BackendWeb.ConnCase
 
-  describe "List" do
+  describe "categories" do
     test "list all categories", %{conn: conn} do
       conn = get(conn, Routes.category_path(conn, :index))
       assert json_response(conn, 200) == []
+    end
+
+    test "should create a category", %{conn: conn} do
+      category = %{name: "Sport", description: "rogerio777"}
+      conn = post(conn, Routes.category_path(conn, :create, category: category))
+      assert %{"id" => id} = json_response(conn, 201)
+
+      conn = get(conn, Routes.category_path(conn, :show, id))
+
+      assert %{"id" => id, "description" => "rogerio777", "name" => "SPORT"} =
+               json_response(conn, 200)
     end
   end
 end
